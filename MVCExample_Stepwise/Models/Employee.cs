f058ec0d4eec13169546df4f230abc2f1e5d6060
lynnsamuelson <lynn.samuelson@gmail.com>
2016-05-03 10:11:29 -0500

    Added Models of Database Tables

---
+++ MVCExample_Stepwise/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCExample_Stepwise.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public String Name { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
+}
